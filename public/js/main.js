a3da935cd1c2453469a911af896b24983cfcdac0
Thinh Duy <thinh.nd.60cntt@ntu.edu.vn>
2022-09-20 16:55:27 +0700

    change url

--- public/js/main.js
+++ public/js/main.js
@@ -8,7 +8,7 @@ const {username, room } = Qs.parse(location.search, {
     ignoreQueryPrefix: true,
 })
 
-const socket = io("https://realtime-chat-app-dusky.vercel.app/");
+const socket = io("https://realtime-chat-app-eosin.vercel.app/");
 
 //join room
 socket.emit("joinRoom", {username, room});
