a1b519ca7e0d4d5354584e8b06d5154df35dc8b1
Thinh Duy <thinh.nd.60cntt@ntu.edu.vn>
2022-09-20 21:31:25 +0700

    fix deploy

--- public/js/main.js
+++ public/js/main.js
@@ -8,7 +8,7 @@ const {username, room } = Qs.parse(location.search, {
     ignoreQueryPrefix: true,
 })
 
-const socket = io.connect("https://realtime-chat-app-eosin.vercel.app/", {
+const socket = io.connect("https://realtime-chat-app-eosin.vercel.app", {
     transports: ['websocket']
 })
 // const socket = io();
